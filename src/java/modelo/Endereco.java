0944f418a9d0025620cbb676d6c58288e6f5daac
gleywson <gleywson@HMUE00911400.hmue.org.br>
2013-12-06 16:58:49 -0300

    Foi integrado ao cadastro de endereço, o municipio e o bairro (ainda com algumas falhas)

--- src/java/modelo/Endereco.java
+++ src/java/modelo/Endereco.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /**
  *
@@ -19,8 +20,10 @@ public class Endereco implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
-//    private String cidade;
-//    private String bairro;
+    @ManyToOne
+    private Municipio municipio;
+    @ManyToOne
+    private Bairro bairro;
     private String logradouro;
     private String cep;
     private String numero;
@@ -34,21 +37,21 @@ public class Endereco implements Serializable {
         this.id = id;
     }
 
-//    public String getCidade() {
-//        return cidade;
-//    }
-//
-//    public void setCidade(String cidade) {
-//        this.cidade = cidade;
-//    }
-//
-//    public String getBairro() {
-//        return bairro;
-//    }
-//
-//    public void setBairro(String bairro) {
-//        this.bairro = bairro;
-//    }
+    public Municipio getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(Municipio municipio) {
+        this.municipio = municipio;
+    }
+
+    public Bairro getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(Bairro bairro) {
+        this.bairro = bairro;
+    }
 
     public String getLogradouro() {
         return logradouro;
